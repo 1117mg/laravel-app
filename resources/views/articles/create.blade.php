210f08d3301d3dc9b4e4e0b7be07a0d42e8c559c
1117mg <lee_yerim@naver.com>
2025-07-11 15:13:52 +0900

    chore: 유효성 검사 실패 시 입력한 내용 유지

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -7,9 +7,9 @@
             <h1 class="text-2xl">글쓰기</h1>
             <form action="/articles" method="POST" class="mt-3">
                 @csrf
-                <input type="text" name='body' class="block w-full mb-2 rounded">
+                <input type="text" name='body' class="block w-full mb-2 rounded" value="{{ old('body') }}">
                 @error('body')
-                    <p class="text-xs text-red-500 m-3"> {{ $message }} </p>
+                    <p class="text-xs text-red-500 mb-3"> {{ $message }} </p>
                 @enderror
                 <button class="py-1 px-3 bg-black text-white rounded text-xs">저장하기</button>
             </form>
